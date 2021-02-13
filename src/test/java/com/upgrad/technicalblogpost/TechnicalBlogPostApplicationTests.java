0cb7816256f7d763101408775b8f040e105088f2
Baibhav Sinha <baibhav1750.cse18@chitkara.edu.in>
2021-02-14 00:09:19 +0530

    Update TechnicalBlogPostApplicationTests.java

--- src/test/java/com/upgrad/technicalblogpost/TechnicalBlogPostApplicationTests.java
+++ src/test/java/com/upgrad/technicalblogpost/TechnicalBlogPostApplicationTests.java
@@ -1,1 +1,14 @@
+package com.upgrad.technicalblogpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalBlogPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
 
